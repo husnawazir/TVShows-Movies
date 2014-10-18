9981cdc2e636fa8b58aaa3e569710d4d17842b30
Muz visitor <muzvisitor@MacBook-Pro-de-Wan-Muzaffar-Wan-Hashim.local>
2014-10-18 13:42:37 +0800

    Initial Commit

---
+++ TVShows&Movies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TVShows&Movies
+//
+//  Created by Muz visitor on 18/10/14.
+//  Copyright (c) 2014 Muz visitor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TVShows&Movies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TVShows&Movies
+//
+//  Created by Muz visitor on 18/10/14.
+//  Copyright (c) 2014 Muz visitor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
